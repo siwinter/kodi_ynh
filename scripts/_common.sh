4413f2276565817c4189916fbf2c13a75e08885b
siwinter <45730097+siwinter@users.noreply.github.com>
2021-10-07 13:19:10 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+if [ -n "$(uname -m | grep arm)" ]                                                  # -n : lenght of string non zero
+    then                                                                            # if arm architecture :
+        pkg_dependencies="xserver-xorg-legacy xorg dbus-x11 kodi kodi-pvr-hts" 
+        exec_start="/usr/bin/kodi-standalone"
+    else                                                                            # if non arm architecture :
+        pkg_dependencies="xorg xinit dbus-x11 kodi kodi-pvr-hts" 
+        exec_start="/usr/bin/xinit /usr/bin/dbus-launch --exit-with-session /usr/bin/kodi-standalone -- :0 -nolisten tcp vt7"
+fi
